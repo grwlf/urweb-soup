bda38b3db1e4b20f8dbd16e8847b111a7849c856
Sergey Mironov <grrwlf@gmail.com>
2015-02-21 08:39:48 +0000

    Cake.hs update

---
+++ Cake_Soup.hs
@@ -0,0 +1,23 @@
+module Cake_Soup where
+
+import Development.Cake3
+import Development.Cake3.Ext.UrWeb as UW
+import Development.Cake3.Utils.Slice
+
+import qualified Cake_Bootstrap as Bootstrap
+import qualified Cake_URU as Uru
+import qualified Cake_Prelude as Prelude
+import Cake_Soup_P
+
+lib = uwlib (file "lib.urp") $ do
+  library Bootstrap.lib
+  library Uru.lib
+  library Prelude.lib
+  allow url "http://github.com*"
+  allow url "http://impredicative.com*"
+  ur (file "Soup.ur")
+
+main = writeDefaultMakefiles $ do
+  rule $ do
+    phony "all"
+    depend lib
