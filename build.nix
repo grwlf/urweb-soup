555abcaab76d83424464f44dd8b404d3dfef224c
Sergey Mironov <grrwlf@gmail.com>
2016-02-11 13:08:38 +0300

    Update build.nix

--- build.nix
+++ build.nix
@@ -19,13 +19,13 @@ rec {
     };
 
     statements = [
-      (set "allow url 'http://github.com*'")
-      (set "allow url 'http://impredicative.com*'")
-      (set "allow url 'https://camo.githubusercontent.com*'")
+      (set "allow url http://github.com*")
+      (set "allow url http://impredicative.com*")
+      (set "allow url https://camo.githubusercontent.com*")
       (embed-css ./Soup.css)
+      (include ./Analytics.h)
       (obj-cpp-11 ./Analytics.cpp)
       (ffi ./Analytics.urs)
-      (include ./Analytics.h)
       (src1 ./Soup.ur)
     ];
   };
